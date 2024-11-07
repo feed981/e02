093d31e4cdfa62e5b51891af780ea65b4135e8bf
feed01 <iu736120@gmail.com>
2024-11-07 15:39:14 +0800

    黑马程序员/实战微服务项目《黑马头条》/freemarker-入门案例、基础语法

---
+++ heima-leadnews-test/freemaker-demo/src/main/java/com/feed02/FreemakerDemoApplication.java
@@ -0,0 +1,14 @@
+package com.feed02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreemakerDemoApplication
+{
+    public static void main( String[] args )
+    {
+
+        SpringApplication.run(FreemakerDemoApplication.class ,args);
+    }
+}
